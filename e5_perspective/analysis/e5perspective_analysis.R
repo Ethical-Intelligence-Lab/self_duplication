209240de701a9cf5e8468cf62ce354f88ba10eac
juliandefreitas <julzdefreitas@gmail.com>
2024-07-18 14:30:07 +0700

    clean analysis script for e5

--- e5_perspective/analysis/e5perspective_analysis.R
+++ e5_perspective/analysis/e5perspective_analysis.R
@@ -64,33 +64,12 @@ table(data$cond)
 
 p_data <- subset(data, data$cond_name=='perspective')
 
-table(p_data$simultaneous_or_flip)
-prop.table(table(p_data$simultaneous_or_flip))
-
 t.test(data$identity ~ data$cond_name)
 aggregate(data$identity ~ data$cond_name, data = data, FUN = sd)
 cohen.d(data$identity, data$cond_name)
 
-mean(data$identity[data$simultaneous_or_flip == "Two consciousnesses"], na.rm=TRUE)
-mean(data$identity[data$simultaneous_or_flip == "Single consciousness, flipping"], na.rm=TRUE)
-mean(data$identity[data$simultaneous_or_flip == "Single consciousness, simultaneous"], na.rm=TRUE)
-mean(data$identity[data$simultaneous_or_flip == "Single consciousness, split screen"], na.rm=TRUE)
+table(p_data$simultaneous_or_flip)
+prop.table(table(p_data$simultaneous_or_flip))
 
-#====================================================================================
-#                               VISUALIZATION
-#====================================================================================
+# end ====================================================================================
 
-x_scale_labels <- c('basic', 'perspective')
-theme_update(plot.title = element_text(hjust = 0.5))
-
-ggplot(d_plot1, aes(x = cond ,y = simultaneous_or_flip))+
-  stat_summary(fun = mean, position = position_dodge(), geom = "bar", width = 0.5) +
-  coord_cartesian(ylim = c(0, 0.70)) +
-  theme(axis.title.x = element_blank()) + 
-  theme_classic() +
-  scale_x_discrete(breaks = 1:2, labels = x_scale_labels) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  theme(text = element_text(size = 15), axis.text.x = element_text(angle = 45, hjust = 1,vjust = 0.8))+
-  ylab("Proportion of Identifications")+
-  xlab("") +
-  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) 
